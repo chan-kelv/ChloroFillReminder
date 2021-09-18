ff45ef1e38dba429bc93c0ad2cbef761c9748458
chan-kelv <chan.kelv@gmail.com>
2021-09-18 11:50:26 -0700

    init commit

---
+++ app/src/main/java/com/kelvinfocus/chlorofillreminder/ChloroFillReminderApplication.kt
@@ -0,0 +1,8 @@
+package com.kelvinfocus.chlorofillreminder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChloroFillReminderApplication: Application() {
+}
